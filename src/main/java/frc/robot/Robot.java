bc736471b81b9f41e7568f1f578a3a87e69ab22d
WIHSRobotics <asma.razi.neuro.30@gmail.com>
2024-02-18 14:59:31 -0500

    working drive code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,13 @@ import javax.sound.sampled.Port;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.revrobotics.*;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -24,10 +30,16 @@ public class Robot extends TimedRobot {
    * initialization code.
    */
 
-private Spark leftmotor1 = new Spark(0);
-private Spark leftmotor2 = new Spark(1);
-private Spark rightmotor1 = new Spark(2);
-private Spark rightmotor2 = new Spark(3);
+private CANSparkMax leftmotor1 = new CANSparkMax(10, MotorType.kBrushed);
+private CANSparkMax leftmotor2 = new CANSparkMax(8, MotorType.kBrushed);
+private CANSparkMax rightmotor1 = new CANSparkMax(11, MotorType.kBrushed);
+private CANSparkMax rightmotor2 = new CANSparkMax(9, MotorType.kBrushed);
+
+SlewRateLimiter ramp = new SlewRateLimiter(0.75, -0.75, 0);  // first two numbers are your accelaration as a % (just diff direction). DONT TOUCH THE LAST NUMBER
+
+private DifferentialDrive drive = new DifferentialDrive(leftmotor1, rightmotor1); //creating the differential drive object with a left side and right side
+double speed;
+double turn;
 
 private Joystick joy1 = new Joystick(0);
 
@@ -35,7 +47,12 @@ private double startTime;
 
 
 @Override
-  public void robotInit() {}
+  public void robotInit() {
+    leftmotor2.follow(leftmotor1) ;               //sets leftmotor2 to do everything leftmotor 1 does
+    rightmotor2.follow(rightmotor1);              //sets rightmotor2 to do everything rightmotor 1 does
+    rightmotor1.setInverted(true);     //flips the rightside so you don't need to multiply by -1
+    rightmotor2.setInverted(true);
+  }
 
   @Override
   public void robotPeriodic() {}
@@ -65,16 +82,20 @@ private double startTime;
 
   @Override
   public void teleopPeriodic(){
-    double speed = -joy1.getRawAxis(1)+0.6;
-    double turn = joy1.getRawAxis(2)+0.3;
+    speed = ramp.calculate(joy1.getRawAxis(1)); //calculating our ramp while reading our joystick
+    turn = joy1.getRawAxis(0);
+    
+    
+    drive.arcadeDrive(speed, turn);   //acrade drive passes in speed and rotation to your drive motors
 
-    double left = speed+turn;
-    double right = speed-turn;
+    //THIS WAS YOUR OLD CODE
+    // double left = speed+turn;
+    // double right = speed-turn;
 
-    leftmotor1.set(left);
-    leftmotor2.set (left);
-    rightmotor1.set(-right);
-    rightmotor2.set(-right);
+    // leftmotor1.set(left);
+    // leftmotor2.set (left);
+    // rightmotor1.set(-right);
+    // rightmotor2.set(-right);
   }
 
   @Override
